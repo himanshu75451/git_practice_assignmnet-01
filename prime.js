c9951834d630b0af710fdade95be8a9d07b53f94
himanshu75451 <himanshukalal75451@gmail.com>
2022-12-06 21:59:07 +0530

    update done

--- prime.js
+++ prime.js
@@ -1,13 +1,25 @@
 
 
-for(let i=1; i<=50; i++){
+function checkprime(num){
     let factor=0;
-    for(let x=1; x<=i; x++){
-        if(i%x===0){
+    for(let x=1; x<=num; x++){
+        if(num%x===0){
             factor++;
         }
     }
     if(factor==2){
-        console.log(x, "is prime");
+        return true;
+    }else{
+        return false;
     }
+}
+
+
+for(let i=1; i<=50; i++){
+    let ans = checkprime(i);
+if(ans==true){
+    console.log(i,"It is prime");
+}else{
+    console.log(i,"It is not prime")
+}
 }
